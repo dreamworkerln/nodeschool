4411792359aa1cf2ae13604f3d8d3651d56cb600
hatuntcev <hatuntcev@mail.ru>
2015-08-13 13:07:42 +0300

    init

---
+++ 02.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var result = 0;
+//console.log(process.argv);
+for (var i = 2; i < process.argv.length; i++) {
+   result += Number(process.argv[i]);
+}
+
+
+console.log(result);
